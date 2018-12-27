3dc807861fa931bf9c106cdf5556763b125da901
Freek Jagerman <f.jagerman@me.com>
2018-12-27 20:48:56 +0100

    Initial Commit

---
+++ XSLT Studio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XSLT Studio
+//
+//  Created by Freek Jagerman on 27/12/2018.
+//  Copyright © 2018 Jagerman Services. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XSLT Studio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XSLT Studio
+//
+//  Created by Freek Jagerman on 27/12/2018.
+//  Copyright © 2018 Jagerman Services. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
