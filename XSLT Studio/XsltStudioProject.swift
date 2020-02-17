f882c0f47cb548d4ed33b48734db1fd14adc7ae1
Freek Jagerman <f.jagerman@me.com>
2020-02-17 17:42:04 +0100

    Added dark mode in template

--- XSLT Studio/XsltStudioProject.swift
+++ XSLT Studio/XsltStudioProject.swift
@@ -16,7 +16,12 @@ struct XsltStudioProject: Codable {
     var xslt: String = """
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:template match="/"></xsl:template>
+    <xsl:template match="/">
+        <html>
+            <style>:root { color-scheme: light dark; }</style>
+            <h1>Hello</h1>
+        </html>
+    </xsl:template>
 </xsl:stylesheet>
 """
     
